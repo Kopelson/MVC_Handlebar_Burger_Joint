991083ad6f6a7100b7386561b3a6444f4f5de1ae
Kenneth Kopelson <kennethkopelson@gmail.com>
2020-12-02 09:51:35 -0800

    created schema and seeds sql files to initialize burgers_db

---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+-- Drops the burgers_db if it already exists --
+DROP DATABASE IF EXISTS burgers_db;
+
+-- Create the database burgers_db and specified it for use --
+CREATE DATABASE burgers_db;
+
+-- Use the burders_db --
+USE burgers_db;
+
+-- Create the table burgers --
+CREATE TABLE burgers (
+  id int NOT NULL AUTO_INCREMENT,
+  burger_name varchar(100) NOT NULL,
+  devoured BOOLEAN NOT NULL default 0,
+  PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+-- Insert a set of records.
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('California burger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie burger');
+INSERT INTO burgers (burger_name) VALUES ('Classic burger');
+INSERT INTO burgers (burger_name) VALUES ('Western burger');
